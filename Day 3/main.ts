f44bcbff9d21beff9309c1d80c0f3ab024b0d2fa
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-05-27 13:28:09 +0500

    updated

---
+++ Day 3/main.ts
@@ -0,0 +1,16 @@
+// Day 3
+// Q7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Addition:
+console.log(6+2);
+// Subtraction :
+console.log(10-2);
+// Multiplication:
+console.log(4*2);
+// Division:
+console.log(16/2);
+// Q8 - You should create four lines that look like this: console.log(5 + 3) Your output should simply be four lines with the number 8 appearing once on each line.
+// Same as Q7:
+// Q9 - Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favouriteNumber:number = 10;
+let message:string = "My favourite number is"
+console.log(message,favouriteNumber);
