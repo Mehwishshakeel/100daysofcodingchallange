3991246d6d5724ba3835f2116aa0460ac74a90ba
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-06-02 13:59:57 +0500

    updated

---
+++ Day 9/main.js
@@ -0,0 +1,63 @@
+// Day 9
+// Q25:Answer
+// version that passes
+var alian_color = "green";
+if (alian_color === "green") {
+    console.log("You  just earned five points ");
+}
+// version that fails      no output
+var alian_color2 = "red";
+if (alian_color2 === "green") {
+    console.log("just earned five points");
+}
+// Q26:Answer
+// First version
+var alian_color3 = "green";
+if (alian_color3 === "green") {
+    console.log("Players just earned 5 points!");
+}
+else {
+    console.log("Players just earned 10 points!");
+}
+// Second version
+var alian_color4 = "Yellow";
+if (alian_color4 === "green") {
+    console.log("Players just earned 5 points for shooting the alian!!");
+}
+else {
+    console.log("Players just earned 10 points!");
+}
+// Q27:Answer
+// Green alian version
+var alianColor = "Green";
+if (alianColor === "Green") {
+    console.log("You  just earned 5 points.");
+}
+else if (alianColor === "Yellow") {
+    console.log("You just earned 10 points.");
+}
+else if (alianColor === "red") {
+    console.log("You just eraned 15 points.");
+}
+// Yellow alian version
+var alianColor1 = "Yellow";
+if (alianColor1 === "Green") {
+    console.log("You  just earned 5 points.");
+}
+else if (alianColor1 === "Yellow") {
+    console.log("You just earned 10 points.");
+}
+else if (alianColor1 === "red") {
+    console.log("You just eraned 15 points.");
+}
+// Red alian version
+var alianColor3 = "red";
+if (alianColor3 === "Green") {
+    console.log("You  just earned 5 points.");
+}
+else if (alianColor3 === "Yellow") {
+    console.log("You just earned 10 points.");
+}
+else if (alianColor3 === "red") {
+    console.log("You just eraned 15 points.");
+}
---
+++ Day 9/main.ts
@@ -0,0 +1,57 @@
+// Day 9
+// Q25:Answer
+// version that passes
+let alian_color:string = "green";
+if(alian_color === "green"){
+   console.log("You  just earned five points ");
+}
+// version that fails      no output
+let alian_color2 = "red";
+if(alian_color2 === "green"){
+      console.log("just earned five points");
+}
+// Q26:Answer
+// First version
+let alian_color3:string = "green";
+if(alian_color3 === "green"){
+   console.log("Players just earned 5 points!");
+}
+else {
+    console.log("Players just earned 10 points!")
+}
+// Second version
+let alian_color4:string = "Yellow";
+if(alian_color4==="green"){
+    console.log("Players just earned 5 points for shooting the alian!!");
+}
+else{
+    console.log("Players just earned 10 points!")
+}
+// Q27:Answer
+// Green alian version
+let alianColor = "Green";
+if(alianColor === "Green"){
+    console.log("You  just earned 5 points.");
+}else if(alianColor  === "Yellow"){
+    console.log("You just earned 10 points.")
+}else if(alianColor==="red"){
+    console.log("You just eraned 15 points.")
+}
+// Yellow alian version
+let alianColor1 = "Yellow";
+if(alianColor1 === "Green"){
+    console.log("You  just earned 5 points.");
+}else if(alianColor1  === "Yellow"){
+    console.log("You just earned 10 points.");
+}else if(alianColor1 ==="red"){
+    console.log("You just eraned 15 points.");
+}
+// Red alian version
+let alianColor3 = "red"
+if(alianColor3 === "Green"){
+    console.log("You  just earned 5 points.");
+}else if(alianColor3  === "Yellow"){
+    console.log("You just earned 10 points.");
+}else if(alianColor3 ==="red"){
+    console.log("You just eraned 15 points.");
+}
