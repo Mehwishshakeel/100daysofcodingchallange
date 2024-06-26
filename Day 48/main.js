c750a44e828fd5d0eb5eb77bfa4a556a697fbc85
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-06-26 14:38:00 +0500

    updated

---
+++ Day 48/main.js
@@ -0,0 +1,48 @@
+"use strict";
+// day 48
+//Question 142: Create a Promise that resolves with "Hello, World!" after 2 seconds.
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello,World!!");
+        }, 2000);
+    });
+};
+helloWorld()
+    .then((response) => {
+    console.log(response);
+});
+// Question 143: Show how to use the .then() and .catch() methods to handle Promise resolution and rejection.
+let isVictory = false;
+const Cricket = () => {
+    return new Promise((resolve, reject) => {
+        if (isVictory) {
+            setTimeout(() => {
+                resolve("We Won");
+            }, 1500);
+        }
+        else {
+            setTimeout(() => {
+                reject("We LOst");
+            }, 1500);
+        }
+    });
+};
+Cricket()
+    .then((response) => {
+    console.log(response);
+})
+    .catch((err) => {
+    console.log(err);
+});
+// Question 144: Explain the use of the Promise.all() method with an example.
+const promise1 = Promise.resolve(3);
+const promise2 = Promise.resolve(78);
+const promise3 = new Promise((resolve, rejection) => {
+    setTimeout(() => {
+        resolve("mee");
+    }, 2000);
+});
+Promise.all([promise1, promise2, promise3]).then((resolvedValue) => {
+    console.log(resolvedValue);
+});
